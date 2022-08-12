a5af75ec11733dd9cdf80e427a097a7ae5075cad
Freg <Freg@personal.com>
2022-08-12 19:08:57 +0800

    V4

--- V4.0/src/main/java/com/webserver/core/ClientHandler.java
+++ V4.0/src/main/java/com/webserver/core/ClientHandler.java
@@ -3,16 +3,12 @@ package com.webserver.core;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
-/*
-    与客户端完成一次HTTP的交互
-    一问一答，分为三步完成
-    1：解析请求 目的：将浏览器发送的请求内容读取并整理
-    2：处理请求 目的：根据浏览器的请求进行对应的处理工作
-    3：发送响应 目的：将服务器端的处理结果回馈给浏览器
- */
-public class ClientHandler implements Runnable{
+public class ClientHandler implements Runnable {
     private Socket socket;
+    private Map<String,String>headers = new HashMap<>();
 
     public ClientHandler(Socket socket) {
         this.socket = socket;
@@ -21,34 +17,53 @@ public class ClientHandler implements Runnable{
     @Override
     public void run() {
         try {
-            InputStream in = socket.getInputStream();
-            /*
-                获取浏览器发来的请求
-             */
-            int d;
-            StringBuilder builder = new StringBuilder();
-            char pre='a',cur='a';
-            while((d = in.read())!=-1){
-                cur=(char) d;
-                if (pre==13&&cur==10){
+            String line = readLine();
+            System.out.println("请求行:" + line);
+            String[] data = line.split("\\s");
+            String method = data[0];
+            String uri = data[1];
+            String protocol = data[2];
+            System.out.println("请求方式：" + method + '\n' + "抽象路径：" + uri + '\n' + "协议版本：" + protocol);
+            //读取
+            while (true) {
+                line = readLine();
+                if (line.isEmpty()){
                     break;
                 }
-                builder.append(cur);
-                pre=cur;
+                System.out.println(line);
             }
-            String line = builder.toString().trim();
-            System.out.println("请求行"+line);
-            //请求行相关信息
-            String method;//请求方式
-            String uri;//抽象路径
-            String protocol;//协议版本
-            //'\s'正则表达式  去空白字符
-            String[] data = line.split("\\s");
-            method = data[0];uri=data[1];protocol=data[2];
-            System.out.println("请求方式："+method+'\n'+"抽象路径"+uri+'\n'+"协议版本"+protocol);
+            //把消息头根据： 拆成key和value
+            line=readLine();
+            String key;
+            String value;
+
+
+
+
+
+
+
+            System.out.println("headers"+headers);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
+    //按行读取，遇到CRLF结束
+    private String readLine() throws IOException {
+        //对同一个socket调用多次getInputStream方法时，调用的始终是同一个流
+        InputStream in = socket.getInputStream();
+        int d;
+        StringBuilder builder = new StringBuilder();
+        char pre = 'a', cur = 'a';
+        while ((d = in.read()) != -1) {
+            cur = (char) d;
+            if (pre == 13 && cur == 10) {
+                break;
+            }
+            builder.append(cur);
+            pre = cur;
+        }
+        return builder.toString().trim();
+    }
+
 }
